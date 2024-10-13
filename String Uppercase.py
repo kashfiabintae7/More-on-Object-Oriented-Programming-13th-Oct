7338f0a2684ec01c23ccd0316714a439f9a9e3ad
Kashfia <kashfiabintae@gmail.com>
2024-10-13 16:15:26 +0600

    first commit

---
+++ String Uppercase.py
@@ -0,0 +1,19 @@
+class IOString():
+    
+    
+    def __init__ (self):
+        self.str1 = ""
+        
+        
+    def get_String(self):
+        self.str1 = input("Enter a String: ")
+        
+    
+    def print_String(self):
+            print("Result is: ", self.str1.upper())
+            
+            
+strObj = IOString()
+
+strObj.get_String()
+strObj.print_String()
